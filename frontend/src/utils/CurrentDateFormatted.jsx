b4189f7876273df42747a829ff7d6060ad65ac98
Joseph <arroyohernandezjoseph@gmail.com>
2025-01-29 13:22:57 -0500

    add notebook

--- frontend/src/utils/CurrentDateFormatted.jsx
+++
@@ -1,6 +0,0 @@
-import {format} from 'date-fns'
-import {es} from 'date-fns/locale'
-
-export default function CurrentDateFormatted () {
-    return format(new Date(), "dd MMM yyyy",{locale: es});
-}
